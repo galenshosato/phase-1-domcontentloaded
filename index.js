a2013f221e7a20db9e8fb3601774f199a7757d9a
Galen Sho Sato <galen.sato@gmail.com>
2023-01-13 13:16:47 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const newText = document.getElementById('text')
+    newText.textContent = 'This is really cool!'
+    
+})
